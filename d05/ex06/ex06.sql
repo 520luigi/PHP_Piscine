5ee5d0240d9ab223e69a0a966713efe508dfea24
Sheng Zheng <szheng@e1z2r10p7.42.us.org>
2019-01-15 20:09:24 -0800

    add rest of d05 problems and fix up minor spacing on other

--- d05/ex06/ex06.sql
+++ d05/ex06/ex06.sql
@@ -1,4 +1,4 @@
 SELECT title, summary
-  FROM film
-  WHERE LOWER(summary) LIKE '%vincent%'
-  ORDER BY id_film ASC;
+    FROM film
+    WHERE LOWER(summary) LIKE '%vincent%'
+    ORDER BY id_film ASC;
